df19d23fc40df1cd5ac665fae89d4c8d72c9c875
ColinH1127 <colinhussey82@gmail.com>
2025-09-22 22:15:43 -0400

    feat: add auto-copying of generated password with pyperclip and add requirements.txt

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from tkinter import *
 from tkinter import messagebox
-
+import random
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+
+    random_letters = [random.choice(letters) for letter in range(nr_letters)]
+    random_symbols = [random.choice(symbols) for symbol in range(nr_symbols)]
+    random_numbers = [random.choice(numbers) for number in range(nr_numbers)]
+
+
+    password_list = random_letters + random_numbers + random_symbols
+    random.shuffle(password_list)
+    password = "".join(password_list)
+    entry3.delete(0, "end")
+    entry3.insert(0, f"{password}")
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_password():
@@ -10,7 +32,7 @@ def add_password():
     email = entry2.get()
     password = entry3.get()
     if website == "" or email == "" or password == "":
-        messagebox.showerror(title=website, message="All fields must be completed.")
+        messagebox.showerror(title="Oops", message="All fields must be completed.")
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered:\n"
                                                       f"Email: {email}\n"
@@ -21,7 +43,7 @@ def add_password():
                 data.write(f"{website} | {email} | {password}\n")
                 entry1.delete(0, "end")
                 entry3.delete(0, "end")
-            messagebox.showinfo(title=website, message=f"Login data for {website} saved.")
+            messagebox.showinfo(title=website, message=f"Login data for {website} saved")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -47,7 +69,7 @@ label3 = Label(text="Password:")
 label3.grid(row=3, column=0)
 entry3 = Entry(width=18)
 entry3.grid(row=3, column=1)
-button3 = Button(text="Generate Password", width=15)
+button3 = Button(text="Generate Password", width=15, command=generate_password)
 button3.grid(row=3, column=2)
 button4 = Button(text="Add", width=35, command=add_password)
 button4.grid(row=4, column=1, columnspan=2)
